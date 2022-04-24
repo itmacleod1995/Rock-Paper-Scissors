7bd1ba2107571ec62a03b6e1434c9099e95c0fd6
Ian <itmacleod@email.wm.edu>
2022-04-24 12:42:11 -0400

    Added starting prompt and game loop

---
+++ main.py
@@ -0,0 +1,6 @@
+import random
+
+game = True
+while game:
+    computerChoice = [0, 1, 2]
+    choice = input("What do you choose? Type 0 for Rock, 1 for Paper, or 2 for Scissors. Type q to quit: ")
